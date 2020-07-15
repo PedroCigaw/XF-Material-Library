fb45f37de66d71eaf518e8167195d6978e00f385
Pedro Puado <pedro@cigaw.co.uk>
2020-07-15 09:40:42 +0100

    Add sample and bump build version

--- Samples/MaterialMvvmSample/ViewModels/MaterialTextFieldViewModel.cs
+++ Samples/MaterialMvvmSample/ViewModels/MaterialTextFieldViewModel.cs
@@ -1,9 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace MaterialMvvmSample.ViewModels
 {
     public class MaterialTextFieldViewModel : BaseViewModel
     {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                Set(ref _name, value);
+                base.OnPropertyChanged(nameof(HasError));
+                base.OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public bool HasError
+        {
+            get
+            {
+                return string.IsNullOrEmpty(_name);
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                if (HasError)
+                {
+                    return "Name is required!";
+                }
+
+                return string.Empty;
+            }
+        }
+
         public MaterialTextFieldViewModel()
         {
         }
